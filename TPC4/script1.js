e2c77d9d7cc4209547a2099a3dee18d2030a3004
brunomachado97374 <a97374@alunos.uminho.pt>
2024-03-11 15:14:34 +0000

    TPC4

---
+++ TPC4/script1.js
@@ -0,0 +1,23 @@
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+// Next/previous controls
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
+}
+
+// Thumbnail image controls
+function currentSlide1(n) {
+  showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides-sports");
+  if (n > slides.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex1-1].style.display = "block";
+}
